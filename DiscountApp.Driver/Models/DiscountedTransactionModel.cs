761908e4e81a5ab57222de5436b6c1f0db4398fe
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-08-04 21:24:51 +0300

    rename provider to carrier

--- DiscountApp.Driver/Models/DiscountedTransactionModel.cs
+++ DiscountApp.Driver/Models/DiscountedTransactionModel.cs
@@ -8,20 +8,20 @@ namespace DiscountApp.Driver.Models;
 /// </summary>
 /// <param name="Date">Date of the transaction</param>
 /// <param name="Size">Package size</param>
-/// <param name="Provider">Shipment provider</param>
+/// <param name="Carrier">Shipment carrier</param>
 /// <param name="DiscountedPrice">Price after discount</param>
 /// <param name="Discount">discount</param>
 public record DiscountedTransactionModel(
     DateOnly Date, 
     PackageSize Size, 
-    Carrier Provider, 
+    Carrier Carrier, 
     decimal DiscountedPrice, 
     decimal Discount)
 {
     public override string ToString()
     {
         var size = Size.GetDisplayNameOrDefault();
-        var provider = Provider.GetDisplayNameOrDefault();
+        var provider = Carrier.GetDisplayNameOrDefault();
         var discountText = Discount is 0 ? "-" : $"{Discount:0.00}";
         return $"{Date:yyyy-MM-dd} {size} {provider} {DiscountedPrice:0.00} {discountText}";
     }
